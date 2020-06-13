d2768812995435cf3520c1c18212ddd05991eb57
lameeus <laurens.meeus@ugent.be>
2020-06-13 13:12:44 +0200

    All kind of double checks (e.g. alphabetical, indexing) if list is correct.

--- main.py
+++ main.py
@@ -36,17 +36,37 @@ def doc_reader(filepath):
     return body     # lists the tables, rows, columns, enters, "letters" (but this is just a string)
 
 
+def single_line(b, s=';'):
+    if isinstance(b, list):
+        return s.join([single_line(b_i).strip() for b_i in b])
+    else:
+        return b
+
+
 # Generate table (pandas?)
 def table_gen(body):
 
     d = []
 
+    titles = [a[0].lower().strip() for a in body[0][0]]
+    if titles[0] == '':   # index
+        titles[0] = 'index'
+
+    i_naam = titles.index('kunstenaar')
+    i_titel = titles.index('titel')
+    i_index = 0
+    i_datum = titles.index('datum')
+    i_typeformaat = titles.index('type en formaat')
+    i_afbeelding = titles.index('afbeelding')     # Ignore!
+
+    def check_if_title(l):
+        return single_line(l[1]).lower() == titles[1]
+
+    assert single_line(body[1]) == '', body[1]
+
     for i, l in enumerate(body[0]):
-        if i == 0:
-            titles = [c[0] for c in l]
 
-            # TODO check when it is title
-
+        if check_if_title(l):  #
             continue
 
         else:
@@ -57,26 +77,51 @@ def table_gen(body):
                 if 0 and (len(c) > 1):
                     print(c)  # Enters
 
-                if i_c == 0:    # naam
+                if i_c in (i_naam, i_titel):    # naam
                     row_i = c[0]    # Only before enter
+                    # Remove "birth/death"
+                    row_i = row_i.split("(")[0].strip()
+
+                elif i_c == i_index:    # Index!
+                    row_i = int(single_line(c))
+
+                elif i_c in (i_datum, i_typeformaat):
+                    row_i = single_line(c, ' ').lower()  # Shouldn't contain higher cases!
+
+                elif i_c == i_afbeelding:
+                    continue    # Do nothing
+
                 else:
-                    row_i = ';'.join(c)
+                    # Should be single line
+
+                    row_i = single_line(c, ' ')
 
                 row[titles[i_c]] = row_i
 
             d.append(row)
 
-        # TODO remove
-        # if i >= 2:
-        #     break
-
     df = pd.DataFrame(d, columns=titles)
 
-    print(df)
+    # Check if index is ok:
+    for i, df_i in df.iterrows():
+        if  df_i['index'] != i + df['index'][0]:
+            print("at", df_i['index'], 'should be:', i + df['index'][0])
 
-    body[1] # TODO
+    # TODO check alphabetical
+    list_lastfirstname = [a.lower() for a in list(df['kunstenaar'])]
+    list_lastfirstname_sorted = sorted(list_lastfirstname)
+    # To lower case
 
-    # for
+    for i, a in enumerate(list_lastfirstname):
+        if a == '':
+            print(i, a)
+
+    for i, (a, b) in enumerate(zip(list_lastfirstname, list_lastfirstname_sorted)):
+        if a != b:
+            print("Error")
+            print(i+1, a, '!=', b)
+
+    # print(df)
 
     l_titles = []
     for i, df_i in df.iterrows():
@@ -85,9 +130,14 @@ def table_gen(body):
         except Exception as e:
             print(e)
             s = 'FAIL'
-        l_titles.append(s)
+
+        assert ';' not in s, s
+
+        l_titles.append({'index': i+1, 'onderschrift':s})
+
     df_titles = pd.DataFrame(l_titles)
-    df_titles.to_csv('C:/Users/Laurens_laptop_w/Downloads/corpus.csv', index=True, sep=';')
+    # df_titles.to_csv('C:/Users/Laurens_laptop_w/Downloads/corpus.csv', index=True, sep=';')
+    df_titles.to_excel(r'C:/Users/Laurens_laptop_w/Downloads/corpus.xlsx', index=False)
 
     return df
 
@@ -98,7 +148,7 @@ def caption_gen(x, debug=False):
         print(x)
 
     # First name, last name
-    s_name = x['KUNSTENAAR']
+    s_name = x['kunstenaar']
 
     if s_name.strip() == '':
         print("Failed to make title", x)
@@ -108,9 +158,9 @@ def caption_gen(x, debug=False):
     name_last, name_first = map(str.strip, s_name0.split('(')[0].split(',', 1))
     s0 = name_first + ' ' + name_last
 
-    s_titel = x['TITEL']
+    s_titel = x['titel']
 
-    s_type = x['TYPE EN FORMAAT']
+    s_type = x['type en formaat']
 
     s_type.split(',')
 
@@ -119,24 +169,29 @@ def caption_gen(x, debug=False):
     else:
         s_mat_formaat = s_type
 
-    s_bewaarplaats = x['BEWAARPLAATS/EIGENAAR']
+    s_bewaarplaats = x['bewaarplaats/eigenaar']
     if s_bewaarplaats == '/':
         s_col = 'bewaarplaats onbekend'
     else:
         s_col = s_bewaarplaats
 
-    x_dat = x['DATUM'].strip()
+    x_dat = x['datum'].strip()
     s_datum = 'datum onbekend' if x_dat == 's.d.' else x_dat
 
     # Naam, titel, datum, materiaal, formaat, collectie
     s = f'{s0}, {s_titel}, {s_datum}, {s_mat_formaat}, {s_col}'
 
-    print(s)
+    # print(s)
+
+    assert ';' not in s, s
 
     return s
 
 
 if __name__ == '__main__':
-    body = doc_reader('C:/Users/Laurens_laptop_w/Downloads/CorpusOverzichtKunstwerkenV2.docx')
+    f1 = 'CorpusKunstwerkenPerKunstenaarV2 (1).docx'
+    f2 = 'CorpusOverzichtKunstwerkenV4_2.docx'
+    # TODO also 'per_kunstenaar'
+    body = doc_reader('C:/Users/Laurens_laptop_w/Downloads/' + f2)
 
     df = table_gen(body)
